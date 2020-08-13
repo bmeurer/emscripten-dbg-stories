9aea18ae20a220b0e6bd23513df49174476b3ab7
Benedikt Meurer <bmeurer@google.com>
2020-08-13 09:55:40 +0200

    feat: switch to explicit Emscripten versions
    Use Emscripten 1.40.1 in the build.sh script now.

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+EMSCRIPTEN_VERSION="1.40.1"
+
 TEMPDIR=`mktemp -d`
 (cd ${TEMPDIR} &&
  git clone https://github.com/emscripten-core/emsdk.git &&
  cd emsdk &&
- ./emsdk install latest &&
- ./emsdk activate latest) || exit $?
+ ./emsdk install ${EMSCRIPTEN_VERSION} &&
+ ./emsdk activate ${EMSCRIPTEN_VERSION}) || exit $?
 source ${TEMPDIR}/emsdk/emsdk_env.sh
 make || exit $?
 rm -rf ${TEMPDIR}
